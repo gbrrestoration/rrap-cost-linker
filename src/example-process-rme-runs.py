c2a4b28da78cc7a12e6bab063922339f5e39e09f
Rose Crocker <r.crocker@aims.gov.au>
2025-07-15 16:44:33 +1000

    Adjust example for parallelisation

--- src/example-process-rme-runs.py
+++ src/example-process-rme-runs.py
@@ -1,14 +1,37 @@
 import cost_calculations as cc
 import process_RME_data as prd
-import pandas as pd
+import time
+import math
+import multiprocessing as mp
 
 # Filepath to RME runs to process
-rme_files_path = "path to RME output files"
-
+rme_files_path = "C:\\Users\\rcrocker\\Documents\\Github\\ReefModEngine.jl\\sandbox\\test_results_econ_metrics"
+# econ_test_results_eff_study_domain
 # Number of sims for metrics sampling (default includes ecological and expert uncertainty in RCI calcs)
-nsims = 10
-# Create metric datafiles for economics modelling and extract filename for intervention key
-int_keys_fn = prd.create_economics_metric_files(rme_files_path, nsims)
-
+nsims = 100
 # Create cost datafiles for the intervention run ids in ID_key
-cc.calculate_costs(int_keys_fn, nsims)
+ncores = 4
+nbatches = math.ceil(nsims/ncores)
+
+# Create metric datafiles for economics modelling and extract filename for intervention key
+#start = time.perf_counter()
+int_keys_fn = prd.create_economics_metric_files(rme_files_path, nsims, nbatches, ncores=ncores, metrics=[prd.rci])
+#end = time.perf_counter()
+#elapsed_1 = end - start
+#print(f'Time taken: {elapsed_1:.6f} seconds')
+
+def calc_costs_para(it_num, int_keys_fn, n_sims):
+    return cc.calculate_costs(int_keys_fn, n_sims, iter_id=it_num)
+
+# calc_costs_para(0, int_keys_fn,  nbatches)
+# breakpoint()
+# start = time.perf_counter()
+if __name__ == "__main__":
+    pool = mp.Pool(ncores)
+    result = [pool.apply(calc_costs_para, args=(iter_id, int_keys_fn, nbatches)) for iter_id in range(ncores)]
+    pool.close()
+    pool.join()
+
+# end = time.perf_counter()
+# elapsed_2 = end - start
+# print(f'Time taken: {elapsed_2:.6f} seconds')

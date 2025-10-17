65b5cd8a36ed2580a3e252d9467dd3c39f13c0b4
Takuya Iwanaga <takuyai@gmail.com>
2025-10-17 18:24:44 +1100

    Clarify example

--- examples/example_process_rme_runs.py
+++ examples/example_process_rme_runs.py
@@ -1,7 +1,7 @@
 import cost_eco_model_linker as ceml
 
 # Filepath to RME runs to process
-rme_files_path = "./sandbox/data/eco_linker_example"
+rme_files_path = "path to RME results"
 
 # Number of sims for metrics sampling (default includes ecological and expert uncertainty in RCI calcs)
 nsims = 10
@@ -10,4 +10,5 @@ nsims = 10
 int_keys_fn, filepaths = ceml.create_economics_metric_files(rme_files_path, nsims)
 
 # Create cost data files for the intervention run ids in ID_key
+# Assumes Cost Model spreadsheets are in same directory as this script.
 result_paths = ceml.calculate_costs(int_keys_fn, nsims, "3.5.5 CA Deployment Model", "3.7.0 CA Production Model")

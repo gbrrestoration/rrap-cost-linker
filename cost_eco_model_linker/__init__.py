d4356c99f17b9c029ab1cf082e3d0d170a12bebe
Takuya Iwanaga <takuyai@gmail.com>
2025-10-23 18:19:22 +1100

    Begin work standardizing parallel processing code
    Remove use of mutable defaults
    
    Begin standardizing docstrings
    
    Apply formatting

--- cost_eco_model_linker/__init__.py
+++ cost_eco_model_linker/__init__.py
@@ -1,4 +1,7 @@
 from .setup_results import setup_dirs
 from .process_RME_data import create_economics_metric_files
 from .cost_calculations import calculate_costs
+
+from .parallel_cost_sampling import para_sample_econ, calc_costs_para
+
 from .runner import evaluate

4531a4cbecf8b7200dc4c1401ba3abdfd8a32caa
Takuya Iwanaga <takuyai@gmail.com>
2026-01-06 17:36:38 +1100

    Auto-format

--- cost_eco_model_linker/setup_results.py
+++ cost_eco_model_linker/setup_results.py
@@ -4,11 +4,14 @@ from os.path import join as path_join
 from collections import OrderedDict
 from dataclasses import dataclass
 
-RESULT_DIRS = OrderedDict({
-    "cost_dir": "cost_outputs",
-    "econ_dir": "econ_outputs",
-    "intervention_keys_dir": "intervention_keys"
-})
+RESULT_DIRS = OrderedDict(
+    {
+        "cost_dir": "cost_outputs",
+        "econ_dir": "econ_outputs",
+        "intervention_keys_dir": "intervention_keys",
+    }
+)
+
 
 # Create immutable instances
 @dataclass(frozen=True)
@@ -17,7 +20,8 @@ class OutputStores:
     econ_dir: str
     intervention_keys_dir: str
 
-def setup_dirs(base_dir):
+
+def setup_dirs(base_dir) -> OutputStores:
     """
     Prep directories to store outputs at user-defined location.
     """

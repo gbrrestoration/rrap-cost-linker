f17f4b7b31526939e2a429d960bb098877c64235
Takuya Iwanaga <takuyai@gmail.com>
2026-01-07 23:24:51 +1100

    Expose `default_uncertainty_dict`

--- cost_eco_model_linker/__init__.py
+++ cost_eco_model_linker/__init__.py
@@ -4,4 +4,5 @@ from .cost_calculations import calculate_costs
 
 from .parallel_cost_sampling import para_sample_econ, post_process_costs
 
+from .calculate_metrics import default_uncertainty_dict
 from .runner import evaluate, parallel_evaluate

d685162720a5635858fb98a7899b6baf33d53242
Takuya Iwanaga <takuyai@gmail.com>
2026-04-07 12:02:51 +1000

    Improved excel handlers

--- cost_eco_model_linker/handlers/excel_handlers.py
+++ cost_eco_model_linker/handlers/excel_handlers.py
@@ -1,25 +1,59 @@
 import os
 import win32com.client as w32client
+import pywintypes
 
 
-def open_excel(fp):
-    # win32com Excel interface can only open files using their absolute paths
+def open_excel(fp, xlapp=None):
+    """
+    Open an Excel workbook.
+
+    Parameters
+    ----------
+    fp : str
+        Path to the workbook file.
+    xlapp : Excel.Application, optional
+        An existing Excel application instance to use. If None, a new
+        isolated instance is created via DispatchEx. Pass an existing
+        instance when opening multiple workbooks to avoid running several
+        Excel processes simultaneously (which causes RPC_E_DISCONNECTED).
+
+    Returns
+    -------
+    xlapp : Excel.Application
+    wb : Workbook
+    """
     wb_file_path = os.path.abspath(fp)
 
-    # Open workbook
-    xlapp = w32client.DispatchEx("Excel.Application")
-    xlapp.Interactive = False
-    xlapp.Visible = False
-    xlapp.DisplayAlerts = False
+    if xlapp is None:
+        xlapp = w32client.DispatchEx("Excel.Application")
+        xlapp.Interactive = False
+        xlapp.Visible = False
+        xlapp.DisplayAlerts = False
 
     wb = xlapp.Workbooks.Open(wb_file_path)
 
     return xlapp, wb
 
 
-def close_excel(xlapp, wb):
-    wb.Close(SaveChanges=False)
-    xlapp.Quit()
+def close_excel(xlapp, wb, quit_app=True):
+    """
+    Close a workbook and optionally quit the Excel application.
+
+    Parameters
+    ----------
+    xlapp : Excel.Application
+    wb : Workbook
+    quit_app : bool
+        If True (default), quit the Excel application after closing the
+        workbook. Pass False when the application hosts other open workbooks
+        that should remain alive.
+    """
+    try:
+        wb.Close(SaveChanges=False)
+    except pywintypes.com_error:
+        pass  # wb may be stale if reset_workbook was called; xlapp.Quit() handles cleanup
+    if quit_app:
+        xlapp.Quit()
 
 
 def reset_workbook(xlapp, wb, fp):

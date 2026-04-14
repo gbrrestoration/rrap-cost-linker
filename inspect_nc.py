625d5fdb7ce8eaef87f390e3aa02f8cac247323c
Daniel Tan <156630392+DanTanAtAims@users.noreply.github.com>
2026-04-14 13:17:12 +0800

    feat: add support for ADRIA

--- inspect_nc.py
+++
@@ -1,7 +0,0 @@
-import netCDF4 as nc
-try:
-    ds = nc.Dataset('C:/users/dtan/data/exported_rme_results/results.nc')
-    print(f"timesteps: {len(ds.variables['timesteps'])}")
-    print(f"locations: {len(ds.variables['locations'])}")
-except Exception as e:
-    print(f"Error: {e}")
